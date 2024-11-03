66c03a11798b31ef481af56bc927c069ddbafd58
igor-prys <photo.prys@gmail.com>
2024-11-03 16:20:38 +0100

    Setup logger,fix DriverSingleton

--- src/test/java/com/epam/ta/driver/DriverSingleton.java
+++ src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -3,7 +3,10 @@ package com.epam.ta.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Optional;
 
 public class DriverSingleton {
 
@@ -12,12 +15,22 @@ public class DriverSingleton {
     private DriverSingleton() {
     }
 
+    private static String getBrowser() {
+       return Optional.ofNullable(System.getProperty("browser")).orElse("chrome");
+    }
+
     public static WebDriver getDriver() {
         if (null == driver) {
-            switch (System.getProperty("browser")) {
+            switch (getBrowser()) {
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
+                    break;
+                }
+                case "edge": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new EdgeDriver();
+                    break;
                 }
                 default: {
                     WebDriverManager.chromedriver().setup();
